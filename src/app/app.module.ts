b3d0b64a36a5b8790c701a42c795b242a2465763
Pratik Kaje <kajepratik@gmail.com>
2024-12-02 22:17:14 +0400

    added topMenu, mainMenu and productListComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { TopHeaderComponent } from './top-header/top-header.component';
+import { TopMenuComponent } from './header/top-menu/top-menu.component';
+import { MainMenuComponent } from './header/main-menu/main-menu.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    TopHeaderComponent
+    TopHeaderComponent,
+    TopMenuComponent,
+    MainMenuComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+  product = {
+    name: 'iPhone 13 PRO Max',
+    price: 999,
+    color: "Red",
+    discount: 8.5,
+    instock: 5
+  }
+
+  getDiscountedPrice(){
+    return (this.product.price - (this.product.price * this.product.discount/100))
+  }
+}
