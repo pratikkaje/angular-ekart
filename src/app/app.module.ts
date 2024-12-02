9b5e1470ad1df74cd1700a1046a65bf8a6f2e381
Pratik Kaje <kajepratik@gmail.com>
2024-12-02 16:20:42 +0400

    Added top-header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { TopHeaderComponent } from './top-header/top-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    TopHeaderComponent
   ],
   imports: [
     BrowserModule
