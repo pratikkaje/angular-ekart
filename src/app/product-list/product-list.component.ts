728513a22733288382597590e730c3e7783bf167
Pratik Kaje <kajepratik@gmail.com>
2024-12-03 06:18:12 +0400

    Event Binding Example

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -6,16 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
+  itemCount: number = 0;
   product = {
     name: 'iPhone 13 PRO Max',
     price: 999,
     color: "Red",
     discount: 8.5,
-    instock: 0,
+    instock: 10,
     pImage: "/assets/images/iPhone.jpg"
   }
 
   getDiscountedPrice(){
     return (this.product.price - (this.product.price * this.product.discount/100))
+  }
+
+  incrementCartValue(){
+    if(this.itemCount < this.product.instock){
+      this.itemCount++;
+    }
+  }
+
+  decrementCartValue(){
+    if(this.itemCount > 0){
+      this.itemCount--;
+    }
   }
 }
