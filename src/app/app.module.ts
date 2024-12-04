60a1a80b420a2186f87add6a40cba3b1e34bef14
Pratik Kaje <kajepratik@gmail.com>
2024-12-04 21:46:46 +0400

    renamed product-list component to container component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,9 @@ import { HeaderComponent } from './header/header.component';
 import { TopHeaderComponent } from './top-header/top-header.component';
 import { TopMenuComponent } from './header/top-menu/top-menu.component';
 import { MainMenuComponent } from './header/main-menu/main-menu.component';
-import { ProductListComponent } from './product-list/product-list.component';
+import { ContainerComponent } from './container/container.component';
+import { SearchComponent } from './container/search/search.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -15,10 +17,12 @@ import { ProductListComponent } from './product-list/product-list.component';
     TopHeaderComponent,
     TopMenuComponent,
     MainMenuComponent,
-    ProductListComponent
+    ContainerComponent,
+    SearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
